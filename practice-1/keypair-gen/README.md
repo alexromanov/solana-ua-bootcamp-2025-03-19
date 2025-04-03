be29bfa23e1343f3878dca77e48bfda04ef18374
Oleksandr Romanov <al8x.romanov@gmail.com>
2025-04-03 14:09:20 +0300

    add logging and README

---
+++ practice-1/keypair-gen/README.md
@@ -0,0 +1,13 @@
+# Practice - Week 1
+
+Including:
+- function to generate Solana keypair
+- function to load keypair from the .env
+- function to print keypair to console
+- function to check the balance for an key from the .env
+- function to generate keypair with private key of specific prefix
+
+## How to run
+- `cargo run` - to show default output
+- `RUST_LOG=info cargo run` - to show INFO logs
+- `RUST_LOG=debug cargo run` - to show DEBUG logs
--- practice-1/keypair-gen/src/main.rs
+++ practice-1/keypair-gen/src/main.rs
@@ -1,24 +1,28 @@
 use dotenv::dotenv;
 use solana_sdk::{bs58, pubkey::Pubkey, signature::Keypair, signer::Signer};
-use std::{env, time::Instant};
+use std::env;
 use solana_client::rpc_client::RpcClient;
+use log::{info, debug};
+use std::time::Instant;
 
 fn main() {
-    println!("\nGenerate Solana keypair\n");
+    env_logger::init();
+    info!("Generate Solana keypair");
     let generated_key = generate_keypair();
     print_keypair(&generated_key);
 
-    println!("\nLoad Solana keypair from .env\n");
+    info!("Load Solana keypair from .env");
     let load_key = load_keypair();
     print_keypair(&load_key);
 
-    println!("\nCheck balance\n");
+    info!("Check balance of the configured address");
     let balance = check_balance(&load_key.pubkey());
     println!("Balance for the wallet at address: {} is {} SOL", load_key.pubkey(), balance);
 
-    println!("\nGenerate key with prefix\n");
-    let prefix = "Al";
-    generate_keypair_with_prefix(prefix);
+    let prefix = "A";
+    info!("Generate key with prefix: {prefix}");
+    let generated = generate_keypair_with_prefix(prefix);
+    print_keypair(&generated);
 }
 
 fn print_keypair(keypair: &Keypair) {
@@ -34,15 +38,18 @@ fn print_keypair(keypair: &Keypair) {
 
 fn generate_keypair() -> Keypair {
     let keypair = Keypair::new();
+    debug!("Keypair is generated: {:?}", keypair);
     keypair
 }
 
 fn load_keypair() -> Keypair {
     dotenv().ok();
 
+    debug!("Reading Base58 SECRET_KEY from .env");
     let private_key_b58 = env::var("SECRET_KEY")
         .expect("Solana private key is not set in .env");
 
+    debug!("Getting private key bytes vec from base58");
     let private_key = bs58::decode(private_key_b58).into_vec()
         .expect("Failed to decode base58 key");
 
@@ -51,9 +58,14 @@ fn load_keypair() -> Keypair {
 }
 
 fn check_balance(public_key: &Pubkey) -> f64 {
-    let url = "https://api.devnet.solana.com";
+    dotenv().ok();
+
+    let url = env::var("DEVNET_URL")
+        .expect("DEVNET_URL has not been found in .env");
+
     let client = RpcClient::new(url);
 
+    debug!("Getting the balance from a pubkey: {}", public_key);
     let balance = client.get_balance(public_key)
         .expect("Failed to retrieve a balance from the walllet");
 
@@ -62,7 +74,7 @@ fn check_balance(public_key: &Pubkey) -> f64 {
 }
 
 fn generate_keypair_with_prefix(prefix: &str) -> Keypair {
-    println!("Generating private key with prefix: {}", prefix);
+    debug!("Generating private key with prefix: {}", prefix);
 
     let start = Instant::now();
     let mut attempts = 0;
@@ -74,15 +86,15 @@ fn generate_keypair_with_prefix(prefix: &str) -> Keypair {
         let private_key = bs58::encode(keypair.to_bytes()).into_string();
 
         if private_key.starts_with(prefix) {
-            println!("Key found after {} attempts!", attempts);
-            println!("Private Key (Base58): {}", private_key);
-            println!("Public Key: {}", keypair.pubkey());
-            println!("Took {:.2?} seconds", start.elapsed());
+            debug!("Key found after {} attempts!", attempts);
+            debug!("Private Key (Base58): {}", private_key);
+            debug!("Public Key: {}", keypair.pubkey());
+            debug!("Took {:.2?} seconds", start.elapsed());
             break;
         }
 
         if attempts % 100_000 == 0 {
-            println!("Attempts: {}", attempts);
+            debug!("Attempts: {}", attempts);
         }
     }
     keypair
